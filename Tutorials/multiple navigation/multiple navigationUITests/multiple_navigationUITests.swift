cf8b0454ab6b1c1a3b43752375855ba976938d34
Max <maxphilipli@gmail.com>
2016-02-20 13:27:19 -0500

    Added navigation tutorials

---
+++ Tutorials/multiple navigation/multiple navigationUITests/multiple_navigationUITests.swift
@@ -0,0 +1,36 @@
+//
+//  multiple_navigationUITests.swift
+//  multiple navigationUITests
+//
+//  Created by Max Li on 2/20/16.
+//  Copyright © 2016 Max Li. All rights reserved.
+//
+
+import XCTest
+
+class multiple_navigationUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
