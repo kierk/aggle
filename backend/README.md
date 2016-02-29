5c41285300af28abff745625a1046c36110fa946
Eugene Kolo <eugene@kolobyte.com>
2016-02-28 19:59:56 -0500

    Updating documentation how to run API server. Added easy setup and run

---
+++ backend/README.md
@@ -0,0 +1,16 @@
+## Quickstart
+
+    start.sh setup
+    start.sh <debug|production>
+
+## Installation
+
+    sudo apt-get install python python-dev
+    sudo apt-get install python-pip
+    sudo apt-get install git
+    sudo pip install -r requirements.txt
+
+## Run
+
+    apiserver.py [--debug] [-x <hostname] [-p <port>]
+
---
+++ backend/start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ACTION=$1
+purple='\033[0;35m'
+red='\033[0;31m'
+off='\033[0m'
+
+case $ACTION in
+    setup)
+        # Setup to run the apiserver
+        printf "${purple}[*] Setting server up${off}\n"
+	sudo apt-get install python python-dev
+	sudo apt-get install python-pip
+	sudo apt-get install git
+	sudo pip install -r requirements.txt
+	;;
+    production)
+	# Start production server
+	printf "${purple}[*] Starting remote server for PRODUCTION${off}\n"
+	./apiserver.py -x 0.0.0.0 -p 5000
+	;;
+    debug)
+	# Start local debug server
+	# Use "ssh -X" to get Xforward and then "firefox 127.0.0.1:5000"
+	printf "${purple}[*] Starting local server for DEBUG${off}\n"
+	printf "${purple}Use 'ssh -X' to get Xforward and then 'firefox 127.0.0.1:5000'${off}\n"
+	./apiserver.py -x 127.0.0.1 -p 5000
+	;;
+esac
+
+printf "${purple}[*] Done${off}\n"
+
