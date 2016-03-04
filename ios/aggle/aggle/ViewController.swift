ca10b250a8d73e488d3dbdacb5db5b3ff8e75c9d
Drew Pellitieri <kierke@bu.edu>
2016-03-04 11:17:04 -0500

    Add basic skeleton Xcode proj (combination of tutorials)
    Project skeleton being developed by Max Li and me

---
+++ ios/aggle/aggle/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  aggle
+//
+//  Created by Max Li on 3/3/16.
+//  Copyright © 2016 Max Li. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+//        if (FBSDKAccessToken.currentAccessToken() == nil) {
+//            print("Not loged in..")
+//        } else {
+//            print("Loged in...")
+//        }
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        
+        self.view.addSubview(loginButton)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: -- Facebook Login
+    
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        
+        if error == nil
+        {
+            self.performSegueWithIdentifier("showNew", sender: self)
+        }
+        else
+        {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+//        print("User Loged out...")
+    }
+    
+    
+    
+    
+    
+    
+}
+
---
+++ ios/aggle/aggleTests/aggleTests.swift
@@ -0,0 +1,36 @@
+//
+//  aggleTests.swift
+//  aggleTests
+//
+//  Created by Max Li on 3/3/16.
+//  Copyright © 2016 Max Li. All rights reserved.
+//
+
+import XCTest
+@testable import aggle
+
+class aggleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
