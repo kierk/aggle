724f7f912ba6148ad9fca17460cb6bc60b8de895
Jose Lemus <jlemus@bu.edu>
2016-04-09 14:21:56 -0400

    Can now encode an image into base64. Will pass to firebase soon. Also, in viewcontroller, there is now a check to see if the user has already authenticated on the phone. If he has, it skips the logout button that it was showing before. Else, it takes the user to login.

--- ios/aggle/aggle/PhotoViewController.swift
+++ ios/aggle/aggle/PhotoViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Firebase
 
 class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    internal var base64Image = "";
+    
+     let imageRef = Firebase(url:"https://aggle.firebaseio.com/")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
@@ -17,6 +22,9 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
 
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,13 +32,31 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     @IBOutlet weak var PhotoLibrary: UIButton!
     @IBOutlet weak var Post: UIButton!
     
     @IBAction func PostAction(sender: UIButton) {
+        
+        print("[PostAction] hey");
+        print(self.base64Image);
+        
+        //let users = [authData.uid : userInfo]
+        //usersRef.updateChildValues(users)
+        
+        let postImaged = [self.base64Image : "test"];
+        
+        imageRef.updateChildValues(postImaged);
+        
+        
+        
     }
     
     @IBAction func PhotoLibraryAction(sender: UIButton) {
+        
+        print("[PhotoLibraryAction] hi");
+        
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .PhotoLibrary
@@ -43,6 +69,35 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         ImageDisplay.image=info[UIImagePickerControllerOriginalImage]as?UIImage;dismissViewControllerAnimated(true, completion: nil)
+        
+        print("[imagePickerController] hi");
+        
+        
+        
+        
+        // this stores the UIImage object in temp.
+        var image : UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage;
+        let imageData = UIImagePNGRepresentation(image);
+        
+        
+        // this encodes correctly
+         self.base64Image = imageData!.base64EncodedStringWithOptions(.Encoding64CharacterLineLength);
+        
+        // this is for decoding for later
+        //let decodedData = NSData(self.base64EncodedString: base64String, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
+        
+        
+        
+        // for debugging breakpoints
+        
+        
+        
+        
+        let a = 5;
+        
+
+        
+        
     }
 
     /*
--- ios/aggle/aggle/ViewController.swift
+++ ios/aggle/aggle/ViewController.swift
@@ -19,15 +19,35 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("top function");
+        // FBSDKAccessToken.setCurrentAccessToken(nil)  // for debugging when a new user logs in
+        // FBSDKProfile.setCurrentProfile(nil)
         
         
-        let loginButton = FBSDKLoginButton()
-        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
-        loginButton.center = self.view.center
-        loginButton.delegate = self
         
-        self.view.addSubview(loginButton)
-        print("here, loaded correctly")
+        if (FBSDKAccessToken.currentAccessToken() != nil){  // if user has token, go to main screen
+            print("AccessToken exists");
+            print(FBSDKAccessToken.currentAccessToken().userID)
+            
+            self.performSegueWithIdentifier("showNew", sender: self)
+            
+        }
+        
+        else{   //if user doesn't have token, go here
+            print("AccessToken doesn't exist exists")
+            //print(FBSDKAccessToken.currentAccessToken().userID)
+            
+            let loginButton = FBSDKLoginButton()
+            loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+            loginButton.center = self.view.center
+            loginButton.delegate = self
+            
+            self.view.addSubview(loginButton)
+            print("here, loaded correctly")
+            
+            
+        }
+        
         
     }
     
@@ -39,77 +59,49 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        
-        
-        
-                //*****************Start of what Max had***************
-        
-//        if error == nil
-//        {
-//            self.performSegueWithIdentifier("showNew", sender: self)
-//        }
-//        else
-//        {
-//            print(error.localizedDescription)
-//        }
-        
-            //****************End  of what Max had****************
-        
-        
-        
+
         print("button has been clicked")
         
-        if error != nil {
+        if error != nil {  // This means we have an error
+            print(error.localizedDescription)
             
         } else {
+            
+            print("in login else");
+            
             let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
             
             ref.authWithOAuthProvider("facebook", token: accessToken,
                 withCompletionBlock: { error, authData in
                     
-                    if error != nil {
-                        print("Login failed. \(error)")
-                    }
-                        // if we make it here, user should be logged in and should be added
-                        // to the users table as a registered user
-                    else {
+                    
+                    self.performSegueWithIdentifier("showNew", sender: self) //here new change
+                    print(authData)
+                    print("Logged in! \(authData) The Users uid is \(authData.uid)")
+                    print("And their display name is \(authData.providerData["displayName"])")
+                    print("And their email is \(authData.providerData["email"])")
+                    //_ = String(authData.uid)
                         
-                        self.performSegueWithIdentifier("showNew", sender: self) //here new change
-                        print(authData)
-                        print("Logged in! \(authData) The Users uid is \(authData.uid)")
-                        print("And their display name is \(authData.providerData["displayName"])")
-                        print("And their email is \(authData.providerData["email"])")
+                    let userDisplayName = String(authData.providerData["displayName"])   // probably btter way of doing this
+                        
+                    let userEmail = String(authData.providerData["email"])
+                        
+                    let userInfo = ["Full Name" : userDisplayName, "Email": userEmail] // key is uid
+                        
+                    let usersRef = self.ref.childByAppendingPath("users")
                         
                         
-                        //_ = String(authData.uid)
-                        
-                        let userDisplayName = String(authData.providerData["displayName"])   // probably btter way of doing this
-                        
-                        let userEmail = String(authData.providerData["email"])
-                        
-                        let userInfo = ["Full Name" : userDisplayName, "Email": userEmail] // key is uid
-                        
-                        let usersRef = self.ref.childByAppendingPath("users")
-                        
-                        let users = [authData.uid : userInfo]
-                        usersRef.updateChildValues(users)
-                        
-                    }
+                    let users = [authData.uid : userInfo]
+                    usersRef.updateChildValues(users)
             })
             
         }
-        
-        
-        
     }
     
     
-    
-    
-    
-    
-    
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("in logout");
+        
     }
     
     
