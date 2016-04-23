13741225e6858584ae9e2df2fc49c488ddff2f1a
mzw23 <zweb89@gmail.com>
2016-04-23 17:45:23 -0400

    Warnings, Navigation, and UI Work
    Cleared some warnings. Began adding new icons. Updated Koloda buttons.

--- ios/aggle/aggle/ViewController.swift
+++ ios/aggle/aggle/ViewController.swift
@@ -28,10 +28,21 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             zipText.text = ""
         }
         else{
-            let alert = UIAlertView()
-            alert.message = "Enter a valid zip code please"
-            alert.addButtonWithTitle("Okay")
-            alert.show()
+            let alert = UIAlertController(title: "Warning", message: "Enter a valid zip code please", preferredStyle: .Alert)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+                // ...
+            }
+            alert.addAction(cancelAction)
+            
+            let OKAction = UIAlertAction(title: "Okay", style: .Default) { (action) in
+                // ...
+            }
+            alert.addAction(OKAction)
+            
+            self.presentViewController(alert, animated: true) {
+                // ...
+            }
         }
     }
     
