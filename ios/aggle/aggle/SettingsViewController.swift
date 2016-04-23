484c9612c7233428823afae4662debbd3b13a5bb
Max <maxphilipli@gmail.com>
2016-04-23 12:39:33 -0400

    Settings tweaks

--- ios/aggle/aggle/SettingsViewController.swift
+++ ios/aggle/aggle/SettingsViewController.swift
@@ -40,20 +40,43 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         print(TAG + "viewDidLoad")
         super.viewDidLoad()
+        self.navigationItem.title = "Aggle"
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+        self.navigationController?.navigationBar.barTintColor = UIColor.redColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        self.pic.layer.borderWidth = 4
+        self.pic.layer.borderColor = UIColor.redColor().CGColor
+        self.pic.layer.cornerRadius = pic.frame.size.height/2
+        self.pic.layer.masksToBounds = true;
+        
+        
         load_image(self.user.pic)
         name.text = self.user.name
         zipCode.text = self.user.zip
+        
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: #selector(SettingsViewController.setBttnTouched(_:)))
+        
+        
     }
-
+    
+    
+    func setBttnTouched(sender: UIBarButtonItem) {
+        
+        performSegueWithIdentifier("ViewControllerSegue", sender: self)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     //override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     //}
 }
