8347423a87c49fe2f4603afa433674bb5a09b6d3
Jose Lemus <jlemus@bu.edu>
2016-04-25 20:49:04 -0400

    readded some code for state persistance.

--- ios/aggle/aggle/ViewController.swift
+++ ios/aggle/aggle/ViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  aggle
 //
-//  This is the Login screen (the one w/ Facebook and all that fun stuff). 
-//  It should be renamed into something like LgoinViewController.
-//
 //  Created by Max Li on 3/3/16.
 //  Copyright © 2016 Max Li. All rights reserved.
 //
@@ -16,47 +13,36 @@ import Firebase
 
 let userDefaults = NSUserDefaults.standardUserDefaults()
 
+
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     let TAG = "[ViewController]"
     let user = User.sharedInstance
-    let ref = Firebase(url:"https://aggle2.firebaseio.com/")
-    var mainZipCode : String = ""
     
     @IBOutlet weak var zipText: UITextField!
+    let ref = Firebase(url:"https://aggle2.firebaseio.com/")
+    var mainZipCode : String = ""
     
     @IBAction func enterZip(sender: AnyObject) {
         if (Int(zipText.text!) != nil && zipText.text!.characters.count == 5){
             self.mainZipCode = zipText.text!
             zipText.text = ""
         }
-        else {
-            let alert = UIAlertController(title: "Warning", message: "Enter a valid zip code please", preferredStyle: .Alert)
-            
-            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
-                // ...
-            }
-            alert.addAction(cancelAction)
-            
-            let OKAction = UIAlertAction(title: "Okay", style: .Default) { (action) in
-                // ...
-            }
-            alert.addAction(OKAction)
-            
-            self.presentViewController(alert, animated: true) {
-                // ...
-            }
+        else{
+            let alert = UIAlertView()
+            alert.message = "Enter a valid zip code please"
+            alert.addButtonWithTitle("Okay")
+            alert.show()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /* Make the top bar */
         self.navigationItem.title = "Aggle"
         self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
         self.navigationController?.navigationBar.barTintColor = UIColor.redColor()
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         
-        print(TAG + "viewDidLoad");
+        print(TAG + "[viewDidLoad] hi");
         FBSDKAccessToken.setCurrentAccessToken(nil)  // for debugging when a new user logs in
         FBSDKProfile.setCurrentProfile(nil)
         
@@ -67,16 +53,16 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         } else {   //if user doesn't have token, go here
             FBSDKAccessToken.setCurrentAccessToken(nil)  // for debugging when a new user logs in
             FBSDKProfile.setCurrentProfile(nil)
-        
+            
             print(TAG + "AccessToken doesn't exist exists")
             
-            /* Make a button that asks the user for some Facebook permissions */
             let loginButton = FBSDKLoginButton()
-            loginButton.readPermissions = ["public_profile", "email", "user_friends"] // eugenek: Why are we getting user_friends? lol
+            loginButton.readPermissions = ["public_profile", "email", "user_friends"]
             loginButton.center = self.view.center
             loginButton.delegate = self
             
             self.view.addSubview(loginButton)
+            print(TAG + "here, loaded correctly")
         }
     }
     
@@ -85,62 +71,86 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     }
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print(TAG + "loginButton clicked");
+        print(TAG + "[ViewController/loginButton] hi");
         
-        if error != nil {
+        if error != nil {  // This means we have an error
             print(TAG + error.localizedDescription)
         } else if result.isCancelled {
             //
         } else {
-            print(TAG + "logging the user in");
+            print("TAG + [ViewController/loginButton] in login else");
             
             let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
             
             ref.authWithOAuthProvider("facebook", token: accessToken,
-                withCompletionBlock: { error, authData in
-                    
-                    self.performSegueWithIdentifier("showNew", sender: self) //here new change
-                    
-                    /* (Re)Create a user model with previous data from the database */
-                    self.user.name = String((authData.providerData["displayName"])!)
-                    if (self.mainZipCode == ""){
-                        self.mainZipCode = "02215"
-                    }
-                    self.user.zip = self.mainZipCode
-                    self.user.email = String((authData.providerData["email"])!)
-                    self.user.pic = String((authData.providerData["profileImageURL"])!)
-                    self.user.uid = authData.uid
-                    
-                    // initialize the dicitonary to be stored in userDefaults
-                    let userDict : [String : [String:String]] = [
-                        self.user.uid : ["ZipCode" : self.user.zip, "Full Name" : self.user.name]
-                    ]
-                    
-                    // ------------ BEGIN ------------
-                    // eugenek: Should this not be removed?
-                    // set the userDefault
-                    userDefaults.setObject(userDict, forKey: self.user.uid)
-                    
-                    // first get values for userID, then get values for zipCode
-                    if let object = userDefaults.objectForKey(self.user.uid)?.valueForKey(self.user.uid){
-                        self.mainZipCode = object.objectForKey("ZipCode")! as! String
-                        
-                    }
-                    // ------------ END ---------------
-
-                    let userInfo = ["Full Name" : self.user.name, "Email": self.user.email, "ZipCode": self.user.zip,]
-                    let usersRef = self.ref.childByAppendingPath("UsersDB")
-                    let user = [authData.uid : userInfo]
-                    usersRef.observeSingleEventOfType(.Value, withBlock: { snapshot in
-                        if snapshot.hasChild(self.user.uid) {
-                            // let my man go
-                        } else {
-                            usersRef.updateChildValues(user)
-                        }
-                    })
+                                      withCompletionBlock: { error, authData in
+                                        
+                                        self.performSegueWithIdentifier("showNew", sender: self) //here new change
+                                        
+                                        self.user.name = String((authData.providerData["displayName"])!)
+                                        
+                                        let userDisplayName = String((authData.providerData["displayName"])!)
+                                        let userEmail = String((authData.providerData["email"])!)
+                                        
+                                        if (self.mainZipCode == ""){
+                                            self.mainZipCode = "02215"
+                                        }
+                                        
+                                        
+                                        print(self.mainZipCode)
+                                        self.user.zip = self.mainZipCode
+                                        self.user.email = userEmail
+                                        self.user.pic = String((authData.providerData["profileImageURL"])!)
+                                        let userID = authData.uid
+                                        //userDict.append(["ZipCode" : self.mainZipCode, "Full Name" : userDisplayName])
+                                        
+                                        
+                                        //var userDict = [String : [String : String]]()
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        //* NSUser default logic*//
+                                        
+                                        // initialize the dicitonary to be stored in userDefaults
+                                        let userDict : [String : [String:String]] = [
+                                            userID : ["ZipCode" : self.mainZipCode, "Full Name" : userDisplayName]]
+                                        
+                                        // set the userDefault
+                                        userDefaults.setObject(userDict, forKey: userID)
+                                        
+                                        // first get values for userID, then get values for zipCode
+                                        if let object = userDefaults.objectForKey(userID)?.valueForKey(userID){
+                                            self.mainZipCode = object.objectForKey("ZipCode")! as! String
+                                            print("mainZip is")
+                                            print(self.mainZipCode)
+                                            
+                                        }
+                                        
+                                        
+                                        //* NSUser default logic*//
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        let userInfo = ["Full Name" : userDisplayName, "Email": userEmail, "ZipCode": self.mainZipCode,]
+                                        
+                                        let usersRef = self.ref.childByAppendingPath("UsersDB")
+                                        print(self.TAG + "zipcode is " + self.mainZipCode)
+                                        
+                                        User.sharedInstance.zip = self.mainZipCode
+                                        
+                                        print(self.TAG + User.sharedInstance.zip)
+                                        
+                                        let users = [authData.uid : userInfo]
+                                        usersRef.updateChildValues(users)
+                                        
             })
         }
     }
+    
     
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
         print(TAG + "in logout");
